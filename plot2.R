7c4f624d24116b9f5d75c05ce24fa30cd99f4da0
Nucleusis <nucleus@clovermail.net>
2016-01-10 21:49:11 +0200

    add files

--- plot2.R
+++ plot2.R
@@ -4,8 +4,13 @@ if (!file.exists("household_power_consumption.txt")){
     classes <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
     data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = classes, nrows = 2075259, comment.char = "")
     data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+    #Sys.setlocale("LC_TIME", "C")
+    datetime <- strptime(with(data, paste(Date, Time)), format = "%d/%m/%Y %H:%M:%S")
+
+    png("plot2.png", bg = "transparent")
+
+    plot(datetime, data$Global_active_power, type = "l", xlab = "", ylab = "")
+    title(ylab = "Global Active Power (kilowatts)")
     
-    png("plot1.png", bg = "transparent")
-    hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
     dev.off()
 }
---
+++ plot4.R
@@ -0,0 +1,29 @@
+if (!file.exists("household_power_consumption.txt")){
+  message("No data in the working directory")
+} else {
+    classes <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+    data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = classes, nrows = 2075259, comment.char = "")
+    data <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+    #Sys.setlocale("LC_TIME", "C")
+    datetime <- strptime(with(data, paste(Date, Time)), format = "%d/%m/%Y %H:%M:%S")
+
+    png("plot4.png", bg = "transparent")
+    par(mfcol = c(2, 2))
+    
+    plot(datetime, data$Global_active_power, type = "l", xlab = "", ylab = "")
+    title(ylab = "Global Active Power")
+      
+    plot(datetime, data$Sub_metering_1, type = "n", xlab = "", ylab = "")
+    points(datetime, data$Sub_metering_1, type = "l", col = "black")
+    points(datetime, data$Sub_metering_2, type = "l", col = "red")
+    points(datetime, data$Sub_metering_3, type = "l", col = "blue")
+    legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col = c("black", "red", "blue"), bty = "n")
+    title(ylab = "Energy sub metering")
+    
+    plot(datetime, data$Voltage, type = "l", ylab = "")
+    title(ylab = "Voltage")
+    
+    with(data, plot(datetime, Global_reactive_power, type = "l"))
+    
+    dev.off()
+}
